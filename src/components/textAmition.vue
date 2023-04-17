6c983fde41d7497acf40d3fe00538a41b9ad973b
yincongcong <yincongcong@wsecar.com>
2023-04-17 18:01:58 +0800

    fix

--- src/components/textAmition.vue
+++ src/components/textAmition.vue
@@ -13,8 +13,8 @@
     span {
         color: #333;
         background: linear-gradient(45deg,blue,red,green) no-repeat;
-        background-size: 0 2px;
         background-position: bottom left;
+        background-size: 0 2px;
         transition: 1s;
         &:hover{
             background-size: 100% 2px;

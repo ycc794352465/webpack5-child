f30ee921c259670585cbf7d1fa51f98f49d032d8
yincongcong <yincongcong@wsecar.com>
2023-04-17 17:15:16 +0800

    fix

--- src/view/test/index.vue
+++ src/view/test/index.vue
@@ -11,6 +11,26 @@ export default {
     components:{
         TextAmition
     },
+    beforeCreate() {
+        console.log('beforeCreate')
+    },
+    created(){
+        console.log('created')
+    },
+    setup() {
+        // fetch('https://www.baidu.com').then(res=>{
+        //     console.log(11111111111)
+        // })
+        console.log('setup')
+     
+        const script=document.createElement('script');
+        script.src=`https://api-uat1.wsfmall.com/api/mobile/categorybyparentid?parentId=0`; // 你的url
+        document.body.appendChild(script)
+        script.onload=()=>{
+            document.body.removeChild(script);
+        }
+        
+    }
 }
 </script>
 
